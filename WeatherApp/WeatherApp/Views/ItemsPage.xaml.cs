4b18dd13187889063357e599ab13ec31ccc0dce0
Svitlana Riezvykh <sriez@softserveinc.com>
2018-05-29 00:02:49 +0300

    ItemsPageXaml added SearchPageXaml added UI completed

---
+++ WeatherApp/WeatherApp/Views/ItemsPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using WeatherApp.Models;
+using WeatherApp.Views;
+using WeatherApp.ViewModels;
+
+namespace WeatherApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new ItemsViewModel();
+        }
+        
+        async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new SearchPage()));
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ItemsListView.SelectedItem = null;
+
+            if (viewModel.LocationInfos.Count == 0)
+                viewModel.LoadItemsCommand.Execute(null);
+        }
+
+        private void LocationDelete_Clicked(object sender, EventArgs eventArgs)
+        {
+            if (ItemsListView.SelectedItem is LocationInfo selectedItem) viewModel.DeleteItemsCommand.Execute(selectedItem);
+            ItemsListView.SelectedItem = null;
+        }
+
+        private void UpdateWeather_Clicked(object sender, EventArgs e)
+        {
+            ItemsListView.SelectedItem = null;
+            viewModel.LoadItemsCommand.Execute(null);
+        }
+    }
+}
---
+++ WeatherApp/WeatherApp/Views/SearchPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using WeatherApp.Models;
+using WeatherApp.ViewModels;
+
+namespace WeatherApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchPage : ContentPage
+    {
+        private SearchViewModel Item;
+
+        public SearchPage()
+        {
+            InitializeComponent();
+
+            BindingContext = Item = new SearchViewModel();
+        }
+
+        private async void SearchWeatherBtn_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(zipCodeEntry.Text))
+            {
+                await Item.SearchForLocation(zipCodeEntry.Text);
+                SerchResultLabel.Text = "";
+                getLocationBtn.Text = "Search Again";
+
+                if (Item.LocationInfos.Count == 0)
+                {
+                    SerchResultLabel.Text = "There is no results for searched code";
+                }
+            }
+        }
+
+        private async void OnLocationSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            if (!(args.SelectedItem is LocationInfo item))
+                return;
+
+            MessagingCenter.Send(this, "AddItem", item);
+
+            await Navigation.PopModalAsync();
+
+            SerchedItemsListView.SelectedItem = null;
+        }
+
+        private async void Back_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
