e7d996cf7ba7b80c195d5421ed5f89754dccd9b8
Svitlana Riezvykh <sriez@softserveinc.com>
2018-05-30 01:55:40 +0300

    UITests project added (Run with Appium)

---
+++ UITests/Driver/WinAppDriver.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Diagnostics;
+
+namespace UITests.Driver
+{
+    public class WinAppDriver
+    {
+        public const string WinAppDriverUri = "http://127.0.0.1:4723";
+        private Process process;
+
+        public WinAppDriver()
+        {
+        }
+
+        public void StartDriver()
+        {
+            var processInfo = new ProcessStartInfo("cmd.exe", "/c " + "\"C:\\Program Files (x86)\\Windows Application Driver\\WinAppDriver.exe\"");
+            process = Process.Start(processInfo);
+        }
+
+        public WindowsDriver<WindowsElement> GetSessionWithRetry(DesiredCapabilities appCapabilities, int retryCount)
+        {
+            WindowsDriver<WindowsElement> DriverInstance = null;
+            for (; retryCount > 0; retryCount--)
+            {
+                try
+                {
+                    DriverInstance = new WindowsDriver<WindowsElement>(new Uri(WinAppDriverUri), appCapabilities, TimeSpan.FromMinutes(1));
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+            }
+
+            return DriverInstance;
+        }
+
+        public void CloseDriver()
+        {
+            process.CloseMainWindow();
+        }
+    }
+}
---
+++ UITests/TestCases/BaseWeatherAppTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Diagnostics;
+using UITests.Driver;
+
+namespace UITests
+{
+    [TestFixture]
+    public abstract class BaseWeatherAppTest
+    {
+        private const string WearherAppAutomationId = "0d599d98-d9a7-4453-9f7d-b76ccfa9e3b9_2jv79xvh9a28c!App";//";
+        public const string WinAppDriverUri = "http://127.0.0.1:4723";
+        private WinAppDriver WinAppDriver = new WinAppDriver();
+        protected WindowsDriver<WindowsElement> DriverInstance;
+
+        [OneTimeSetUp]
+        public void StartDriver()
+        {
+            WinAppDriver.StartDriver();
+        }
+
+        [SetUp]
+        public void SetupSession()
+        {
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("app", WearherAppAutomationId);
+            appCapabilities.SetCapability("platformName", "Windows");
+
+            DriverInstance = WinAppDriver.GetSessionWithRetry(appCapabilities, 10);
+
+            DriverInstance.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [TearDown]
+        public void QuitSession()
+        {
+            DriverInstance.Quit();
+        }
+
+        [OneTimeTearDown]
+        public void CloseDriver()
+        {
+            WinAppDriver.CloseDriver();
+        }
+    }
+}
