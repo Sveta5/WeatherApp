de35df702dafbeace37056d57e70044832f7ada5
Svitlana Riezvykh <sriez@softserveinc.com>
2018-05-29 13:29:07 +0300

    Tests are fixed

--- WeatherApp/WeatherApp/Services/Weather/WeatherApiClient.cs
+++ WeatherApp/WeatherApp/Services/Weather/WeatherApiClient.cs
@@ -30,31 +30,16 @@ namespace WeatherApp.Services.Weather
 
         public async Task<IEnumerable<LocationInfo>> SearchLocation(string postalCode)
         {
-            //var urlWithParametr = string.Format(LocationSearchByPostalCodeUrl, postalCode);
-            //var locationInfos = await RequestResult<IEnumerable<LocationInfo>>(urlWithParametr);
-            //return locationInfos;
-            return new Collection<LocationInfo>
-            {
-                new LocationInfo
-                {
-                    Key = "6787_PC155"+ new Random().Next(1, 1000),
-                    Country = new Country {EnglishName = "United States"},
-                    LocationName = "State College",
-                    PostalCode = "16802"
-                }
-            };
+            var urlWithParametr = string.Format(LocationSearchByPostalCodeUrl, postalCode);
+            var locationInfos = await RequestResult<IEnumerable<LocationInfo>>(urlWithParametr);
+            return locationInfos;
         }
 
         public async Task<WeatherInfo> GetCurrentWeather(string cityKey)
         {
-            //var urlWithParametr = string.Format(CurretnWeatherByLocationKeyUrl, cityKey);
-            //var weatherInfo = await RequestResult<WeatherInfo[]>(urlWithParametr);
-            //return weatherInfo[0];
-            return new WeatherInfo
-            {
-                Temperature = new Temperature { Metric = new Metric { Value = new Random().Next(1, 30).ToString(), Unit = "Sunny" } },
-                WeatherText = "Sunny"
-            };
+            var urlWithParametr = string.Format(CurretnWeatherByLocationKeyUrl, cityKey);
+            var weatherInfo = await RequestResult<WeatherInfo[]>(urlWithParametr);
+            return weatherInfo[0];
         }
         private async Task<T> RequestResult<T>(string urlWithParametr)
         {
